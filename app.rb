35f1a1e007e0b5d21827eb457ad1b4c851179a2e
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-21 02:49:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,7 +12,17 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
-
-  # Code your final two routes here:
+  
+  get "/goodbye/:name" do 
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end 
+  
+  get "/multiply/:num1/:num2" do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    "#{@num1 * @num2}"
+    # we will first multiply the number BY integer, then include it in a string - 2-step 
+  end 
 
 end
